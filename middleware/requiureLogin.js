d3cfcfd85f67a6fdf07670cdb9a2213499033834
rajkumarsahu89 <sahukumarraj89@gmail.com>
2023-02-20 18:21:05 +0530

    c4

--- middleware/requiureLogin.js
+++ middleware/requiureLogin.js
@@ -5,7 +5,7 @@ const RegistrationModel=require("../model/auth");
 const requireLogin=(req,res,next)=>{
     const{authorization}=req.headers;
     if(!authorization){
-        return req.status(401).json({error:"Not authorized"});
+        return res.status(401).json({error:"Not authorized"});
     } else {
         const token=authorization.replace("Bearer","");
         jwt.verify(token,"linkedin app",(err,data)=>{
